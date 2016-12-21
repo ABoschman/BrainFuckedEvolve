14a0439f8d3e49b374bc026bd1e2953949922a62
ABoschman <a.boschman@hotmail.com>
2016-12-22 00:55:45 +0100

    Implemented an iterator that returns the 42 different round configurations that make up a complete game of BF Joust.

--- bf_bot_core/src/game/game.rs
+++ bf_bot_core/src/game/game.rs
@@ -12,21 +12,24 @@ use bot::Bot;
 
 /// Compares two bots in a (complete) game and returns the result. 
 pub fn run_complete_game(bot_a: &Bot, bot_b: &Bot) -> GameResult {
-    run_game(bot_a, bot_b, &make_complete_rounds() )
+    run_game(bot_a, bot_b, &AllRounds::new() )
 }
 
-/// Compares two bots in a game, using the given game parameters. Returns the result of the game.
+/// Compares two bots in a game consisting of the provided rounds. Returns the result of the game.
 pub fn run_game(bot_a: &Bot, bot_b: &Bot, rounds: &Iterator<Item=RoundParams>) -> GameResult {
+    //TODO:...
     GameResult { bot_a_points: 0, bot_b_points: 0 }
 }
 
-/// The result of a full game of Brainfuck joust. A game consists of multiple rounds.
+/// The result of a game of Brainfuck joust. A game consists of multiple rounds.
 pub struct GameResult {
     bot_a_points: u8,
     bot_b_points: u8,
 }
 
-/// Specifies the parameters of a single round of Brainfuck Jousting.
+/// Specifies the conditions of a single round of Brainfuck Jousting.
+#[derive(Debug)]
+#[derive(PartialEq)]
 pub struct RoundParams {
     tape_length: u8,
     invert_polarity: bool,
@@ -34,20 +37,70 @@ pub struct RoundParams {
 
 //===== Default rounds supplier, used for complete games:
 
-fn make_complete_rounds() -> AllRounds {
-    AllRounds {}
-}
+const MIN_TAPE_LENGTH: u8 = 10;
+const MAX_TAPE_LENGTH: u8 = 30;
 
 /// An iterator that returns all rounds in a complete game, covering all possible tape lengths and both polarities.
 pub struct AllRounds {
-    //TODO:...
+    tape_length: u8,
+    invert_polarity: bool,
+}
+
+impl AllRounds {
+    fn new() -> AllRounds {
+        AllRounds {
+            tape_length: MIN_TAPE_LENGTH,
+            invert_polarity: false
+        }
+    }
+
+    fn current_item(&self) -> RoundParams {
+        RoundParams { tape_length: self.tape_length, invert_polarity: self.invert_polarity }
+    }
+
+    fn update_state(&mut self) {
+        self.tape_length = if self.invert_polarity { self.tape_length + 1 } else { self.tape_length };
+        self.invert_polarity = !self.invert_polarity;
+    }
+
 }
 
 impl Iterator for AllRounds {
     type Item = RoundParams;
 
     fn next(&mut self) -> Option<RoundParams> {
-        //TODO:... spit out 42 rounds, tape lenghts [10-30], both polarities.
-        None
+        if self.tape_length <= MAX_TAPE_LENGTH {
+            let params = self.current_item();
+            self.update_state();
+            Some(params)
+        } else {
+            None
+        }
     }
+
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn allRounds_gives42Rounds() {
+    assert_eq!(AllRounds::new().count(), 42);
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn allRounds_gives21ReversedPolarityRounds() {
+    let all_inverted_rounds = AllRounds::new().filter(| round | { round.invert_polarity });
+    assert_eq!(all_inverted_rounds.count(), 21);
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn allRounds_areAtLeastMinTapeLength() {
+    assert!(AllRounds::new().all(| round | { round.tape_length >= MIN_TAPE_LENGTH }));
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn allRounds_areAtMostMaxTapeLength() {
+    assert!(AllRounds::new().all(| round | { round.tape_length <= MAX_TAPE_LENGTH }));
 }
